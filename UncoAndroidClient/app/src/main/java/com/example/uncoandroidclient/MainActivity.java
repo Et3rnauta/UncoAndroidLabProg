02b5daf9a2c1d6f649f9a0275480f7c3616c278e
MarcosDanielTorres <marcoss.nqn@gmail.com>
2020-08-04 21:28:32 -0300

    acutalicé

--- UncoAndroidClient/app/src/main/java/com/example/uncoandroidclient/MainActivity.java
+++ UncoAndroidClient/app/src/main/java/com/example/uncoandroidclient/MainActivity.java
@@ -2,13 +2,112 @@ package com.example.uncoandroidclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import Conexion.ClientConnector;
+import Conexion.ConnectionRequestHandler;
+import Conexion.ServerConnector;
+import Conexion.ServerController;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView status, output;
+
+    private EditText input;
+
+    private Button connect, send, disconnect;
+
+    private ClientConnector connector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        status = (TextView) findViewById(R.id.txtv_status);
+        output = (TextView) findViewById(R.id.txtv_output);
+
+        input = (EditText) findViewById(R.id.ptxt_input);
+
+        connect = (Button) findViewById(R.id.btn_connect);
+        connect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new ConnectButton().execute();
+            }
+        });
+
+        send = (Button) findViewById(R.id.btn_send);
+        send.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new SendButton().execute(input.getText().toString());
+            }
+        });
+
+        disconnect = (Button) findViewById(R.id.btn_disconnect);
+        disconnect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new DisconnectButton().execute();
+            }
+        });
+
+        connector = new ClientConnector(ServerController.getIpAdress());
+
+        System.out.println("Your IP Address is: " + ServerController.getIpAdress());
+    }
+
+
+    class ConnectButton extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            if(connector.startConnection(new TestClientHandler())){
+               status.setText("Connected!");
+            }else{
+                status.setText("Couldn't connect!");
+            }
+
+
+            return null;
+        }
+
+        private class TestClientHandler extends ConnectionRequestHandler {
+            @Override
+            public String handle(String msgReq) {
+                output.setText(msgReq);
+                return null;
+            }
+        }
+    }
+
+    class SendButton extends AsyncTask<String,Void,Void> {
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String message = strings[0];
+            connector.makeRequest(message);
+            return null;
+        }
+    }
+
+    class DisconnectButton extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+           if( connector.endConnection()){
+               status.setText("Not connected!");
+           }else{
+               status.setText("YOU CANT DISCONNECT, now you are mine!" +
+                       "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+           }
+            return null;
+        }
     }
 }
